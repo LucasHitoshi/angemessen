7fe371132b7c5c200846c4ab87626b142e01b7e4
LucasHitoshi <lucasogawarec@gmail.com>
2023-04-27 07:49:41 -0300

    Aula 27/04/2023
    Co-authored-by: VictorTureckVaz <VictorTureckVaz@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 function getRandomBallSize(idBall) {
     let ball = document.getElementById(`ball-${idBall}`);
-    ball.setAttribute("style", `width: ${Math.floor(Math.random() * 10) * 5}rem`);
+    ball.setAttribute("style", `width: ${(Math.floor(Math.random() * 5) + 5) * 5}rem`);
 }
